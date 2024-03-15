706ab3e7b006ee4c439cd50a23918bfd4abc1298
Christopher TruLove <christopher.trulove@carvana.com>
2024-03-15 12:37:38 -0700

    First attempt at deploy to live

--- app.js
+++ app.js
@@ -17,3 +17,17 @@ app.get("/api", (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
+/*
+Original cPanel server
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
+*/
