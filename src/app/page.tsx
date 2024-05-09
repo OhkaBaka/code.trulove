c77278cc884065c9d79693f5578042a685f04126
Christopher TruLove <christopher.trulove@carvana.com>
2024-05-09 13:51:07 -0700

    Tweaks for content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://jsfiddle.net/OhkaBaka/e60Ld4vn/63/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -98,6 +98,23 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Lets do some pattern matching!
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/OhkaBaka"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            GitHub{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+          The guts of various other projects plus the latest iterations of this site.
           </p>
         </a>
       </div>
