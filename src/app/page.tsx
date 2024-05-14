ce64bae822fc859f91b896e58dbc7c5d8f1d532e
Christopher TruLove <c.t.trulove@gmail.com>
2024-05-14 16:18:55 -0700

    needed a title... now it needs a BETTER title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,24 @@ export default function Home() {
     <Bax bgSrc={"/images/bg.jpg"} />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 p-6 text-black backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border bg-gray-200 lg:p-4">
           Okay so I am just rebuilding this in Next.js at the moment... so it is a mess.
         </p>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 text-black backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border bg-gray-200 lg:p-4">
           My background is a dynamically generated array of boxes that will always fill the viewport... there is a future in its design, hover and motion effects, but for now. Neat! Boxes!
         </p>
+      </div>
+  
+      <div className="mt-0 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-center w-full font-black text-4xl text-green-950">
+        <h1 className="w-full col-span-4">
+          Christopher Trulove's Portfolio Playground
+        </h1>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://leeduser.buildinggreen.com/credit/NC-v4.1/IPc1"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -34,7 +40,7 @@ export default function Home() {
 
         <a
           href="https://www.amazon.com/b?ie=UTF8&node=18014079011"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -51,7 +57,7 @@ export default function Home() {
 
         <a
           href="https://www.genealogybank.com/explore/newspapers/all/usa/washington?pq=1&prebuy=no&intver=&CCPRODCODE=&s_trackval=&s_referrer=&s_siteloc=&kbid=69919"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -68,7 +74,7 @@ export default function Home() {
 
         <a
           href="https://jsfiddle.net/OhkaBaka/04hn1nvu/5/"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -85,7 +91,7 @@ export default function Home() {
 
         <a
           href="https://jsfiddle.net/OhkaBaka/e60Ld4vn/63/"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -102,7 +108,7 @@ export default function Home() {
 
         <a
           href="https://github.com/OhkaBaka"
-          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent p-2 m-2 transition-colors bg-neutral-600/50 hover:border-gray-150 hover:bg-gray-50/30 hover:text-green-950"
           target="_blank"
           rel="noopener noreferrer"
         >
